7d96b43aa7dc2ffbd349ed847e4793260ab4b51c
Melih Ulukoz <melih_ulukoz@hotmail.com>
2025-01-23 06:20:12 +0300

    Update [KAM] Isometric Box.lua

--- [KAM] Isometric Box.lua
+++ [KAM] Isometric Box.lua
@@ -1,53 +1,3 @@
---[[---------------------------------------------------------------------
-                                        __
- __                                    /\ \__         __
-/\_\    ____    ___     ___ ___      __\ \ ,_\  _ __ /\_\    ___
-\/\ \  /',__\  / __`\ /' __` __`\  /'__`\ \ \/ /\`'__\/\ \  /'___\
- \ \ \/\__, `\/\ \L\ \/\ \/\ \/\ \/\  __/\ \ \_\ \ \/ \ \ \/\ \__/
-  \ \_\/\____/\ \____/\ \_\ \_\ \_\ \____\\ \__\\ \_\  \ \_\ \____\
-   \/_/\/___/  \/___/  \/_/\/_/\/_/\/____/ \/__/ \/_/   \/_/\/____/
- __                            _          __
-/\ \                         /' \       /'__`\
-\ \ \____    ___   __  _    /\_, \     /\ \/\ \
- \ \ '__`\  / __`\/\ \/'\   \/_/\ \    \ \ \ \ \
-  \ \ \L\ \/\ \L\ \/>  </      \ \ \  __\ \ \_\ \
-   \ \_,__/\ \____//\_/\_\      \ \_\/\_\\ \____/
-    \/___/  \/___/ \//\/_/       \/_/\/_/ \/___/
-      
-  ISOMETRIC BOX GENERATOR 1.0 for Aseprite (https://aseprite.org)
-  Project page: https://darkwark.itch.io/isobox-for-aseprite
-   
-    by Kamil Khadeyev (@darkwark)
-    Twitter: http://twitter.com/darkwark
-    Dribbble: http://dribbble.com/darkwark
-    Website: http://darkwark.com
-
-  (c) 2018, November 
-  All rights reserved or something
-  
-  Features:
-    + Customize X, Y and Z size of the box
-    + Customize colors for each side of the box
-    + Custom Stroke and Highlight colors
-    + Two types of the box: 3px and 2px corner
-  
-  Requirements:
-    + Aseprite 1.2.10-beta2
-    + Color Mode: RGBA
-  
-  Installation:
-    + Open Aseprite
-    + Go to `File → Scripts → Open Scripts Folder`
-    + Place downloaded LUA script into opened directory
-    + Restart Aseprite
-  
-  Usage:
-    + Go to `File → Scripts → [KAM] Isometric Box` to run the script
-    + You can also setup a custom hotkey under `Edit → Keyboard Shortcuts`
-    
------------------------------------------------------------------------]]
-
-
 ---------------------------------------
 -- USER DEFAULTS --
 ---------------------------------------
@@ -59,7 +9,6 @@ local colors = {
   top = app.fgColor,
   left = Color{h=c.hsvHue, s=c.hsvSaturation+0.3, v=c.hsvValue-0.1, a=255},
   right = Color{h=c.hsvHue, s=c.hsvSaturation+0.3, v=c.hsvValue-0.4, a=255},
-  highlight = Color{h=c.hsvHue, s=c.hsvSaturation-0.2, v=c.hsvValue+0.2, a=255}
 }
 
 -- Use 3px corner by default:
@@ -187,56 +136,68 @@ local function drawCube(type, xSize, ySize, zSize, color)
   local centerX = math.floor(app.activeSprite.width/2)
   local centerY = math.floor(app.activeSprite.height/2)
   
-  local a = (type == 1) and 0 or 1
-  local b = (type == 1) and 1 or 0
+  local a, b
+  if (type == 1) then 
+    -- 3 px corner
+    a = 0
+    b = 1
+  elseif (type == 2) then
+    -- 2 px corner
+    a = 1
+    b = 0
+  else
+    -- 4 px corner
+    a = -1
+    b = 2   -- veya b = 2; bu çizim mantığınıza göre değişebilir
+  end
   
   --top plane
+  
   isoLineUpRight(color, centerX-a, centerY, xSize) --bottom right
   isoLineUpLeft(color, centerX, centerY, ySize) --bottom left
-  isoLineUpLeft(color, centerX+xSize*2+b, centerY-xSize, ySize) --top right
-  isoLineUpRight(color, centerX-ySize*2-1, centerY-ySize, xSize) --top left
+  isoLineUpLeft(color, centerX+xSize*2+b, centerY-xSize-1, ySize) --top right
+  isoLineUpRight(color, centerX-ySize*2-1, centerY-ySize-1, xSize) --top left
 
   --bottom plane
   isoLineUpRight(color, centerX-a, centerY+zSize, xSize) --right
   isoLineUpLeft(color, centerX, centerY+zSize, ySize) --left
 
   --vertical lines
-  vLine(color, centerX-a, centerY, zSize) --middle
+  vLine(color, centerX, centerY, zSize) --middle
   vLine(color, centerX-ySize*2-1, centerY-ySize, zSize) --left
   vLine(color, centerX+xSize*2+b, centerY-xSize, zSize) --right
+  vLine(color, centerX+1, centerY, zSize) --middle
 end
 
 
 ------------ Adding Colors: ------------
 
-local function fillCubeSides(topColor, leftColor, rightColor)
+local function fillCubeSides(type, topColor, leftColor, rightColor)
   local centerX = math.floor(app.activeSprite.width/2)
   local centerY = math.floor(app.activeSprite.height/2)
-  
+
   local TRANSPARENT_COLOR = app.pixelColor.rgba(0, 0, 0, 0)
-  
-  -- Top Color
-  floodFill(centerX, centerY-1, TRANSPARENT_COLOR, colorAsPixel(topColor))
-  -- Left Color
-  floodFill(centerX-2, centerY+1, TRANSPARENT_COLOR, colorAsPixel(leftColor))
-  -- Right Color
-  floodFill(centerX+1, centerY+1, TRANSPARENT_COLOR, colorAsPixel(rightColor))
+
+  if (type == 1) then
+    -- 3 px corner offsets
+    floodFill(centerX,     centerY-1, TRANSPARENT_COLOR, colorAsPixel(topColor))
+    floodFill(centerX-2,   centerY+1, TRANSPARENT_COLOR, colorAsPixel(leftColor))
+    floodFill(centerX+1,   centerY+1, TRANSPARENT_COLOR, colorAsPixel(rightColor))
+
+  elseif (type == 2) then
+    -- 2 px corner offsets
+    floodFill(centerX,     centerY-1, TRANSPARENT_COLOR, colorAsPixel(topColor))
+    floodFill(centerX-2,   centerY+1, TRANSPARENT_COLOR, colorAsPixel(leftColor))
+    floodFill(centerX+1,   centerY+1, TRANSPARENT_COLOR, colorAsPixel(rightColor))
+
+  else
+    -- 4 px corner offsets (deneme yanılma ile ayarlanmalı)
+    floodFill(centerX,     centerY-2, TRANSPARENT_COLOR, colorAsPixel(topColor))
+    floodFill(centerX-3,   centerY+2, TRANSPARENT_COLOR, colorAsPixel(leftColor))
+    floodFill(centerX+2,   centerY+2, TRANSPARENT_COLOR, colorAsPixel(rightColor))
+  end
 end
 
-local function addHighlight(type, xSize, ySize, zSize, color)
-  local centerX = math.floor(app.activeSprite.width/2)
-  local centerY = math.floor(app.activeSprite.height/2)
-  
-  -- TYPE1 is for 3px in the middle
-  -- TYPE2 is for 2px in the middle
-  local alt = (type == 1) and 0 or 1
-  
-  isoLineUpRight(color, centerX-alt, centerY, xSize-1)
-  isoLineUpLeft(color, centerX, centerY, ySize-1) 
-  vLine(color, centerX-alt, centerY, zSize-1)
-  
-  app.activeImage:putPixel(centerX-alt, centerY, app.pixelColor.rgba(255, 255, 255, 255))
-end
 
 ---------------------------------------
 -- LAYER MANAGEMENT --
@@ -256,33 +217,45 @@ end
 ---------------------------------------
 local dlg = Dialog("[KAM] Isometric Box")
 dlg   :separator{ text="Size:" }
-      :slider {id="ySize", label="Left:", min=1, max=maxSize.y, value=5}
-      :slider {id="xSize", label="Right:", min=1, max=maxSize.x, value=5}
-      :slider {id="zSize", label="Height:", min=3, max=maxSize.z, value=10}
+      :slider {id="ySize", label="Left:", min=1, max=maxSize.y, value=7}
+      :slider {id="xSize", label="Right:", min=1, max=maxSize.x, value=7}
+      :slider {id="zSize", label="Height:", min=3, max=maxSize.z, value=16}
 
       :separator{ text="Colors:" }
       :color {id="color", label="Stroke:", color = colors.stroke}
       :color {id="topColor", label="Top:", color = colors.top}
       :color {id="leftColor", label="Left:", color = colors.left}
       :color {id="rightColor", label="Right:", color = colors.right}
-      :color {id="highlightColor", label="Highlight:", color = colors.highlight}
 
       :separator()
-      :radio {id="typeOne", label="Corner:", text="3 px", selected=use3pxCorner}
-      :radio {id="typeTwo", text="2 px", selected=not use3pxCorner}
+      :radio {
+        id="typeThree",
+        label="Corner:",
+        text="4 px",
+        selected=(use3pxCorner == 3)
+      }
 
       :separator()
-      :button {id="ok", text="Add Box",onclick=function()
+      :button {
+        id="ok",
+        text="Add Box",
+        onclick=function()
           local data = dlg.data
           app.transaction(function()
-            local cubeType = data.typeOne and 1 or 2
-
+            local cubeType
+            if data.typeOne then
+              cubeType = 1   -- 3 px
+            elseif data.typeTwo then
+              cubeType = 2   -- 2 px
+            else
+              cubeType = 3   -- 4 px (yeni eklediğimiz)
+            end
+      
             newLayer("Cube("..data.xSize.." "..data.ySize.." "..data.zSize..")")
             drawCube(cubeType, data.xSize, data.ySize, data.zSize, data.color)
-            fillCubeSides(data.topColor, data.leftColor, data.rightColor)
-            addHighlight(cubeType, data.xSize, data.ySize, data.zSize, data.highlightColor)
+            fillCubeSides(cubeType, data.topColor, data.leftColor, data.rightColor)
           end)
-          --Refresh screen
+          -- Refresh screen
           app.command.Undo()
           app.command.Redo()
         end
@@ -290,4 +263,3 @@ dlg   :separator{ text="Size:" }
       :show{wait=false}
 
 ---------------------------------------
-
